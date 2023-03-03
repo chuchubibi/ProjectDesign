6ca7b2fcd8c96f30f5b7bc182fc895f7aa18628d
sherwin bayaban <sherace0311@gmail.com>
2023-03-03 12:57:36 +0800

    Modified gunicorn_start.sh

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -4,9 +4,6 @@ rem Activate your virtual environment
 call C:\Users\shera\OneDrive\Desktop\visual studio\finlizedProjectPD02\venv\Scripts\activate
 
 rem Start Gunicorn
-gunicorn main.wsgi:application ^
-    --bind 0.0.0.0:8000 ^
-    --workers 3 ^
-    --log-level=info ^
-    --access-logfile=-
+gunicorn main.wsgi:application --bind 0.0.0.0:8000 --workers 3 --log-level=info --access-logfile=-
+
 
